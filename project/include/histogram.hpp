c0898b29b8cb51c647332eba39a47c31e8f7131c
Anthony Super <anthony@noided.media>
2016-10-10 12:38:26 -0600

    documentation

--- project/include/histogram.hpp
+++ project/include/histogram.hpp
@@ -10,7 +10,8 @@
 #include <errors.hpp>
 
 /**
- * Read a list of integers between 0 and 255, and does some basic stats on them.
+ * A histogram which uses binned values of length 4, to a max value of 255.
+ * It stores the individual bins in two formats: their counts, and normalized.
  */
 class Histogram {
 
@@ -24,24 +25,52 @@ public:
      * The type of the array of NormalizedBinValues we use.
      */
     using NormalizedBinType = std::array<double, binCount>;
-
+    /**
+     * Construct a Histogram from a list of numbers.
+     * All numbers must be between 0 and 255.
+     * \param list the list of numbers to construct a histogram from.
+     */
     Histogram(const std::vector<int> &list);
+    /**
+     * Construct a histogram by merging two histograms.
+     * This adds their binned values together bin-wise, then calculates the
+     * new averages for each bin.
+     */
     Histogram(const Histogram& h1, const Histogram &h2);
+    /**
+     * Default constructor merely constructs a bunch of zero-valued bins.
+     */
     Histogram();
 
+    /**
+     * Print a stem-and-leaf chart to standard output. 
+     * Highly likely to be removed in the future.
+     */
     void printStemAndLeaf() const;
 
+    /**
+     * Obtain a reference to the bins in this histogram
+     */
     inline const BinType& getBins() const { return bins; }
 
+    /**
+     * Obtain a reference to the normalized bins in this histogram.
+     */
     inline const NormalizedBinType& getNormalizedBins() const {
         return normalizedBins;
     }
 
+    /**
+     * Obtain the vector dot product of this histogram with another.
+     */
     double dotProduct(const Histogram &other) const;
 
+    /**
+     * Obtain the minimum sum comparison of this histogram to another.
+     * Note that this is equivalent to the intersection of the two histograms.
+     */
     double minimumSum(const Histogram &other) const;
                
-
     private:
     BinType bins;
     NormalizedBinType normalizedBins;
