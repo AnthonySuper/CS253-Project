c0c2fcc5fc8eed035d1f755b56ee268d02ff5216
Anthony Super <asuper@rams.colostate.edu>
2016-12-04 18:35:08 -0700

    More stuff

--- project/benchmark.sh
+++ project/benchmark.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 FILES=test_files/given/perceptron_train/correctfiles.txt 
-if [ "true" = $1 ]; then
-  valgrind --tool=callgrind ./PA10 $FILES $FILES 3
+if [ $1 = "true" ]; then
+  valgrind --tool=callgrind ./PA10 $FILES $FILES 3;
+
 else
+
   time ./PA10 $FILES $FILES 3
-fi;
+
+fi
