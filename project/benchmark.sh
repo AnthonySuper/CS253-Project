05114a00de38cb5fcab872ac6b8127334585238b
Anthony Super <anthony@noided.media>
2016-12-04 18:32:35 -0700

    Benchmark

--- project/benchmark.sh
+++ project/benchmark.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-time ./PA10 test_files/given/perceptron_train/correctfiles.txt test_files/given/perceptron_train/correctfiles.txt 3
+FILES=test_files/given/perceptron_train/correctfiles.txt 
+if [ "true" = $1 ]; then
+  valgrind --tool=callgrind ./PA10 $FILES $FILES 3
+else
+  time ./PA10 $FILES $FILES 3
+fi;
