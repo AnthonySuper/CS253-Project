a4ad3844b936c9f36226fff2020a80fe9d69fc66
Anthony Super <anthony@noided.media>
2016-12-30 10:24:26 -0700

    continue cleaning

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 git filter-branch --force --index-filter \
-'git rm --cached --ignore-unmatch ./rec_one/*' \
+'git rm --cached --ignore-unmatch ./rec_three/*' \
 --prune-empty --tag-name-filter cat -- --all
