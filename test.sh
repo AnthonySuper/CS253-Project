99eaf487602cdda09fa1e7768e58ebde67be9a03
Anthony Super <anthony@noided.media>
2016-12-30 10:26:26 -0700

    Stage

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 git filter-branch --force --index-filter \
-'git rm --cached --ignore-unmatch ./rec_three/*' \
+'git rm --cached --ignore-unmatch ./rec_one/*' \
 --prune-empty --tag-name-filter cat -- --all
